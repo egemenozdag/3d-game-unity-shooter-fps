81a56da9aecbd20437d9a364c73d420b31bb474a
egemenozdag <31034722+egemenozdag@users.noreply.github.com>
2019-02-15 14:02:03 +0300

    Add files via upload

---
+++ asset/winScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.SceneManagement;
+
+public class winScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+				if(col.gameObject.tag == "Player"){
+					win();	
+				}
+	}
+	void win(){
+			SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+	}
+
+}
